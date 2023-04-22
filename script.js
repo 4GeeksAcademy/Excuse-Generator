2ea89f567ed99922cde5923092ec73203ffee0de
Xomano <129993903+Xomano@users.noreply.github.com>
2023-04-22 01:03:51 +0000

    2.0

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 let who = ["The dog", "My grandma", "His turtle", " My bird"];
-let what = ["ate", "pissed", "crushed", "broke"];
+let what = ["ate", "pissed on", "crushed", "broke"];
 let when = [
   "before the class.",
   "right in time.",
   "when I finished.",
   "during lunch.",
-  "while I was praying."
+  "while I was praying.",
 ];
 
 //Picks a random number from 0 to range
